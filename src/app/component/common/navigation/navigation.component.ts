90b22d574217ac17dfb9a9b89190f99bcd334c6e
thiraviaraj <thiraviaraj>
2020-09-30 10:26:31 +0530

    auth update

--- src/app/component/common/navigation/navigation.component.ts
+++ src/app/component/common/navigation/navigation.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-
-  constructor(public auth: AngularFireAuth) { }
+  authState: Observable<firebase.User>;
+  userDetails: firebase.User = null;
+  constructor(public auth: AngularFireAuth) {
+    this.authState = auth.authState;
+    this.authState.subscribe(
+      (user) => {
+        if (user) {
+          this.userDetails = user;
+          console.log(this.userDetails);
+        }
+        else {
+          this.userDetails = null;
+        }
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
